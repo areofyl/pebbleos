24dab51aa9af5d0bb3900e09aa2840781609245c
areofyl <areofyl@users.noreply.github.com>
2026-03-01 22:11:58 -0800

    add exception handler

---
+++ exception.c
@@ -0,0 +1,17 @@
+void print(const char *s);
+void print_hex(unsigned long val);
+
+void exception_handler(unsigned long esr, unsigned long elr,
+                       unsigned long far) {
+  print("EXCEPTION\n");
+  print("   ESR: ");
+  print_hex(esr);
+  print("\n   ELR: ");
+  print_hex(elr);
+  print("\n   FAR: ");
+  print_hex(far);
+  print("\n");
+
+  for (;;) {
+  }
+}
