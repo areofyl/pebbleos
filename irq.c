bbc42d51bdede4ed19ac8e6afc5610d1cae28c0d
areofyl <areofyl@users.noreply.github.com>
2026-03-06 20:00:06 -0800

    hook scheduler into timer irq
    timer tick now calls schedule() for preemptive multitasking
    instead of just acking the interrupt

--- irq.c
+++ irq.c
@@ -1,40 +1,38 @@
 #include <stdint.h>
 
-// called when an irq fires. figure out what it was and handle it
+// irq handler
+// returns a stack pointer - if the scheduler switches tasks,
+// this will be different from what we got
 
 uint32_t gic_ack(void);
 void gic_eoi(uint32_t iar);
 void timer_reset(void);
 extern volatile uint64_t tick_count;
+uint64_t *schedule(uint64_t *frame);
 void print(const char *s);
 void print_hex(unsigned long val);
 
-void irq_handler(unsigned long esr, unsigned long elr, unsigned long far) {
-    // dont need these for irqs, theyre for sync exceptions
-    (void)esr;
-    (void)elr;
-    (void)far;
+uint64_t irq_handler(uint64_t frame) {
+    uint64_t *sp = (uint64_t *)frame;
 
     uint32_t iar = gic_ack();
     uint32_t intid = iar & 0x3FF;
 
-    // spurious interrupt, just ignore
     if (intid == 1023)
-        return;
+        return frame;
 
     if (intid == 30) {
-        // timer
         tick_count++;
         timer_reset();
 
-        print("[tick] #");
-        print_hex(tick_count);
-        print("\n");
+        // let the scheduler decide if we should switch
+        sp = schedule(sp);
     } else {
-        print("[irq] unexpected INTID: ");
+        print("[irq] unexpected: ");
         print_hex(intid);
         print("\n");
     }
 
     gic_eoi(iar);
+    return (uint64_t)sp;
 }
